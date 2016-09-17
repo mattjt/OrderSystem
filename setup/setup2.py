63e16145f2055e8c25072c3e60f52899eb63005a
Matt Turi <mturi@mort11.org>
2016-09-17 13:57:11 -0400

    Fixed an issue with mailer debug text formatting

--- setup/setup2.py
+++ setup/setup2.py
@@ -9,12 +9,12 @@ from OrderSystem import db
 from OrderSystem.sql.ORM import Settings, User, Subteam
 from OrderSystem.utilities.Helpers import hash_password
 
-print """
+print ("""
 ----------------------------------------------------------------------------------------------------------------
 IF YOU ENCOUNTER MYSQL RELATED ERRORS WHILE RUNNING THIS SCRIPT, MAKE SURE THAT YOU HAVE CHANGED THE DEFAULT
 SETTINGS FOR MYSQL IN /configuration/mysql.ini !!!
 ----------------------------------------------------------------------------------------------------------------
-"""
+""")
 
 # Empty DB
 db.drop_all()
@@ -27,7 +27,7 @@ db.session.add(Settings(key="fiscal_year", value=date.today().year))
 
 db.session.add(Subteam("Order System Admin", False))
 db.session.add(
-    User("robotics-osa", "MORT", "Admin", hash_password('admin'), "mort11org@mort11.org", True, True, True, True, True,
+    User("robotics-osa", "MORT", "Admin", hash_password('admin'), "delete_me@mort11.org", True, True, True, True, True,
          1))
 db.session.commit()
-print "Done"
+print ("Done")
