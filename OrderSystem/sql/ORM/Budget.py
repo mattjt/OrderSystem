4db08047561b60ea7f3ae239bad7037825af049e
Matt Turi <mturi@mort11.org>
2016-10-01 14:11:53 -0400

    Fixed SQL schema issue

--- OrderSystem/sql/ORM/Budget.py
+++ OrderSystem/sql/ORM/Budget.py
@@ -4,8 +4,7 @@ from OrderSystem import db
 class Budget(db.Model):
     __tablename__ = "budgets"
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    subteam_id = db.Column(db.Integer, db.ForeignKey('subteams.id'), nullable=False, unique=True)
-    subteam = db.relationship('Subteam', foreign_keys=[subteam_id])
+    subteam_id = db.Column(db.Integer, nullable=False, unique=False)
     dollar_amount = db.Column('dollar_amount', db.Numeric(15, 2), nullable=False)
     fiscal_year = db.Column('fiscal_year', db.Integer, nullable=False)
 
