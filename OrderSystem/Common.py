fd0fe5f5b8c6e9543bb1463eecb9d686cad489af
Matt Turi <mturi@mort11.org>
2016-10-30 20:08:51 -0400

    Added better debugging for orders

--- OrderSystem/Common.py
+++ OrderSystem/Common.py
@@ -4,3 +4,4 @@ APP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 CONFIG_ROOT = APP_ROOT + "/configuration/"
 TEMPLATE_ROOT = APP_ROOT + "/OrderSystem/templates"
 STATIC_ROOT = APP_ROOT + "/OrderSystem/static"
+DEBUG_MODE = True
