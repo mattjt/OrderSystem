dd03499b472d50ec2ce32347e5e7fb8f3311684c
Matt Turi <mturi@mort11.org>
2017-01-10 17:34:55 -0500

    Fixed #33

--- OrderSystem/routing/OrderBackend.py
+++ OrderSystem/routing/OrderBackend.py
@@ -4,6 +4,7 @@ from flask import render_template, request, url_for, flash, redirect, abort
 from flask_classy import FlaskView, route
 from flask_login import current_user, login_required
 from sqlalchemy import and_
+from sqlalchemy.orm.exc import UnmappedInstanceError
 
 from ErrorHandler import get_current_user
 from OrderSystem import db, Common
@@ -392,9 +393,12 @@ class PendingOrders(FlaskView, CRUDBase):
 
         # If the user is an admin, or is a member of that subteam, they can approve the order
         if current_user.is_admin or current_user.subteam_ref.id == order_to_approve.part_for_subteam:
-            order_to_approve.pending_approval = False
-            db.session.commit()
-            flash("Successfully approved order!", 'success')
+            if not order_to_approve == None:
+                order_to_approve.pending_approval = False
+                db.session.commit()
+                flash("Successfully approved order!", 'success')
+            else:
+                flash("You tried to approve/deny a non-existent order", 'error')
         else:
             flash("You can't approve an order that isn't made on your subteam's behalf!")
         return redirect(url_for('PendingOrders:index'))
@@ -411,9 +415,12 @@ class PendingOrders(FlaskView, CRUDBase):
 
         # If the user is an admin, or is a member of that subteam, they can approve the order
         if current_user.is_admin or current_user.subteam_ref.id == order_to_deny.part_for_subteam:
-            db.session.delete(order_to_deny)
-            db.session.commit()
-            flash("Successfully denied order!", 'warning')
+            try:
+                db.session.delete(order_to_deny)
+                db.session.commit()
+                flash("Successfully denied order!", 'warning')
+            except UnmappedInstanceError:
+                flash("You tried to approve/deny a non-existent order", 'error')
         else:
             flash("You can't deny an order that isn't made on your subteam's behalf!", 'error')
 
