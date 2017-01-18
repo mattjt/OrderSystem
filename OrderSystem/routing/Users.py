d13caf4b5a895fa498d23bcbed3f52d699db49f4
Matt Turi <mturi@mort11.org>
2017-01-17 19:53:13 -0500

    Fixed more username/password fields

--- OrderSystem/routing/Users.py
+++ OrderSystem/routing/Users.py
@@ -6,7 +6,7 @@ from CRUDBase import CRUDBase
 from OrderSystem import db
 from OrderSystem import forms
 from OrderSystem.sql.ORM import User
-from OrderSystem.utilities.Helpers import verify_password, hash_password, flash_errors, strip_non_ascii
+from OrderSystem.utilities.Helpers import verify_password, hash_password, flash_errors
 
 
 class Users(FlaskView, CRUDBase):
@@ -27,9 +27,9 @@ class Users(FlaskView, CRUDBase):
     def update(self):
         form = forms.ChangePassword(request.form)
         if form.validate_on_submit():
-            if verify_password(current_user.password, strip_non_ascii(form.current_password.data)):
+            if verify_password(current_user.password, form.current_password.data):
                 user = db.session.query(User).filter(User.id == current_user.id).first()
-                user.password = hash_password(strip_non_ascii(form.new_password.data))
+                user.password = hash_password(form.new_password.data)
                 db.session.commit()
                 flash("Successfully changed password!", 'success')
                 return redirect(url_for('auth.logout'))
